da06da3530b277ad9d91c1b7838d85726cefc03e
Quyu Kong <kongquyu@gmail.com>
2020-07-14 23:51:14 +0000

    Minor fixes

--- R/PL.R
+++ R/PL.R
@@ -6,8 +6,8 @@ get_param_names.hawkes_PL <- function(model) {
 
 get_ampl_likelihood.hawkes_PL <- function(model) {
   paste(
-    'sum {cn in 1..HL} (  (1 - 0^(L[cn]-J0[cn]-1)) *  (sum {i in J0[cn]+1..L[cn]-1} (log(K)',
-    '+ log(sum {j in 1..i-1} ((time[cn,i] - time[cn,j] + c) ^ (-1-theta)) + 1e-100))',
+    'sum {cn in 1..HL} (  (1 - 0^(L[cn]-J0[cn]-1)) *  (sum {i in J0[cn]+1..L[cn]-1} (',
+    'log(K * sum {j in 1..i-1} ((time[cn,i] - time[cn,j] + c) ^ (-1-theta)) + 1e-100))',
     ')',
     '- K * sum {i in 1..L[cn]-1} (( (1 / c)^theta - ( 1 / (time[cn,L[cn]] - time[cn,i] + c))^theta )) / theta);'
   )
@@ -24,8 +24,8 @@ get_param_names.hawkes_mPL <- function(model) {
 
 get_ampl_likelihood.hawkes_mPL <- function(model) {
   paste(
-    'sum {cn in 1..HL} (  (1 - 0^(L[cn]-J0[cn]-1)) *  (sum {i in J0[cn]+1..L[cn]-1} (log(K)',
-    '+ log(sum {j in 1..i-1} (magnitude[cn,j]^beta * (time[cn,i] - time[cn,j] + c) ^ (-1-theta)) + 1e-100))',
+    'sum {cn in 1..HL} (  (1 - 0^(L[cn]-J0[cn]-1)) *  (sum {i in J0[cn]+1..L[cn]-1} (',
+    'log(K * sum {j in 1..i-1} (magnitude[cn,j]^beta * (time[cn,i] - time[cn,j] + c) ^ (-1-theta)) + 1e-100))',
     ')',
     '- K * sum {i in 1..L[cn]-1} (magnitude[cn,i]^beta * ( (1 / c)^theta - ( 1 / (time[cn,L[cn]] - time[cn,i] + c))^theta )) / theta);'
   )
